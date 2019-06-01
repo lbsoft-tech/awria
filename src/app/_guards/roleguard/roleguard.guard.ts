45a65f7bd29b1769629a90dd85835ec0e383bb7f
Muhammad Ahmed Abbasi <abbasi477@live.com>
2019-06-01 17:48:15 +0500

    auth + role guards

--- src/app/_guards/roleguard/roleguard.guard.ts
+++ src/app/_guards/roleguard/roleguard.guard.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RoleguardGuard implements  {
-  
+export class RoleguardGuard implements CanActivate {
+
+  constructor(private _authService: AuthService, private _router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    const user = this._authService.getCurrentUser();
+
+    if (user.role === route.data.role) {
+      return true;
+    }
+
+    // navigate to not found page
+    this._router.navigate(['/home-page/login']);
+    return false;
+  }
+
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { first } from 'rxjs/operators';
+import { User } from 'src/app/_models/user/user';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  readonly baseUrl= 'http://localhost:3000';
-  constructor(private httpClient: HttpClient) { }
+  readonly baseUrl = 'http://localhost:3000';
+  private User: User;
 
+  constructor(private httpClient: HttpClient) {
+    this.User = null;
+  }
+
+  isAuthenticated() {
+    return this.User != null;
+  }
+
+  getCurrentUser() {
+    return this.User;
+  }
 
   login(data) {
     return this.httpClient.post(this.baseUrl + '/auth/login', data);
